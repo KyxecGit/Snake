050d584d3bcfbca247cf69b6bc74cd2002a877ac
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-08-10 19:06:18 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -26,14 +26,17 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+ 
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
+
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
 
     for segment in snake.segments:
         if segment == snake.head:
@@ -41,5 +44,9 @@ while game_is_on:
         elif snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
+
+
+
+
 
 screen.exitonclick()
